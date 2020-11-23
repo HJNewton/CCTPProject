6687cf6cec9fc0a85e6ff825b97dad9c9ba9883e
Harry <harry3.newton@live.uwe.ac.uk>
2020-11-23 19:56:30 +0000

    Camera switching
    Can press F to change camera

---
+++ CCTP Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    public GameObject mainCamera;
+    public GameObject topCamera;
+    public Light worldLight;
+    bool topActive = false;
+
+    private void Update()
+    {
+        SwitchCamera();
+    }
+
+    void SwitchCamera()
+    {
+        if (topActive)
+        {
+            mainCamera.SetActive(false);
+            topCamera.SetActive(true);
+            worldLight.shadowStrength = 0;
+        }
+
+        if (!topActive)
+        {
+            mainCamera.SetActive(true);
+            topCamera.SetActive(false);
+            worldLight.shadowStrength = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            topActive = !topActive;
+        }
+    }
+}
